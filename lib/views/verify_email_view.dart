a5f4fedb2b3c1dc3c5d39cb7cd12b230811fe549
Jonathan Durinda <jonathan1138@hotmail.com>
2022-09-23 21:42:51 -0400

    step-2
    Another Round, changed name

---
+++ lib/views/verify_email_view.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import '../main.dart';
+
+class VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Verify email'),
+      ),
+      body: Column(
+        children: [
+          const Text('Please verify your email address:'),
+          TextButton(
+            onPressed: () async {
+              final user = FirebaseAuth.instance.currentUser;
+              await user?.sendEmailVerification();
+            },
+            child: const Text('Send email verification'),
+          )
+        ],
+      ),
+    );
+  }
+}
